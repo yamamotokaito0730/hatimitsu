b85d9780c24dffe102bea7a5a277d9e274092b61
yamamotokaito0730 <149446520+yamamotokaito0730@users.noreply.github.com>
2025-04-12 00:32:16 +0900

    カメラの挙動を変更、プレイヤーの移動速度変更

--- hatimitsu/Assets/Script/Camera.cs
+++ hatimitsu/Assets/Script/Camera.cs
@@ -8,7 +8,6 @@ public class Camera : MonoBehaviour
 
     [Header("カメラのオフセット")]
     [SerializeField] private Vector3 m_Offset = new Vector3(0f, 5f, -7f);
-    [SerializeField] private float angle;
 
     [Header("カメラの追従速度")]
     [SerializeField] private float smoothSpeed = 5f;
@@ -22,12 +21,12 @@ public class Camera : MonoBehaviour
         // 目標の位置（ターゲット＋オフセット）
         Vector3 desiredPosition = m_Target.position + m_Offset;
 
-        // スムーズにカメラを移動させる（補間）
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        // スムーズにカメラを移動させる（補間）ダッシュの時使用
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        //transform.position = smoothedPosition;
 
-        transform.position = smoothedPosition;
-
+        transform.position = desiredPosition;
         // ターゲットを常に見つめる
-        transform.LookAt(m_Target.position + Vector3.up * angle); // プレイヤーの頭あたりを見る感じ
+        transform.LookAt(m_Target.position); // プレイヤーの頭あたりを見る感じ
     }
 }

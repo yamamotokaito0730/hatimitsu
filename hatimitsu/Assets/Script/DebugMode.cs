a1e62dbc6219c170106771736134b49042a148b8
ToyamaRyota <tooyamaryouta@gmail.com>
2025-04-24 21:54:38 +0900

    コメントの変更

--- hatimitsu/Assets/Script/DebugMode.cs
+++ hatimitsu/Assets/Script/DebugMode.cs
@@ -27,47 +27,35 @@ public class DebugMode : MonoBehaviour
     [SerializeField] private TextMeshProUGUI posText;
     [SerializeField] private TextMeshProUGUI killCountText;
 
-    /*＞Update関数
-    引数：なし
-    ｘ
-    戻値：なし
-    ｘ
-    概要:更新関数
-    */
-    void Update()
-    {
-        
-    }
-
     /*＞デバッグ更新関数
-    引数1：v3_PlayerPos
-    引数2：f_Speed
-    引数3：n_KillCount 
+    引数1：_PlayerPos
+    引数2：_Speed
+    引数3：_KillCount 
     ｘ
     戻値：なし
     ｘ
     概要:引数から受け取った数値に各テキストを更新する
     */
-    public void UpdateDebugUI(Vector3 v3_PlayerPos, float f_Speed, int n_KillCount)
+    public void UpdateDebugUI(Vector3 _PlayerPos, float _Speed, int _KillCount)
     {
-        posText.text = "Pos" + v3_PlayerPos;
-        speedText.text = "Speed" + f_Speed;
-        killCountText.text = "Count" + n_KillCount;
+        posText.text = "Pos" + _PlayerPos;
+        speedText.text = "Speed" + _Speed;
+        killCountText.text = "Count" + _KillCount;
 
     }
     /*＞デバッグ表示関数
-    引数1：v3_PlayerPos
+    引数：_Show
     ｘ
     戻値：なし
     ｘ
     概要:デバッグUIの表示非表示を切り替える
     */
-    public void ToggleDebugText(bool show)
+    public void ToggleDebugText(bool _Show)
     {
         // nullチェックしながら表示/非表示
-        if (speedText != null) speedText.enabled = show;
-        if (posText != null) posText.enabled = show;
-        if (killCountText != null) killCountText.enabled = show;
+        if (speedText != null) speedText.enabled = _Show;
+        if (posText != null) posText.enabled = _Show;
+        if (killCountText != null) killCountText.enabled = _Show;
     }
 
 
